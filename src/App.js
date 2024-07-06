a80c0bfc4a4606cc93d7ee12139571653c08923b
Mohammed Nisham <mohammednisham22@gmail.com>
2024-07-06 09:50:21 +0530

    fin1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Main from './components/Main';
-import Card from './components/Card';
-import Contributions from './components/Contributions';
-import Contact from './components/Contact';
+import "./App.css";
+import Main from "./components/Main";
+import Card from "./components/Card";
+import Contributions from "./components/Contributions";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="main-container d-flex align-items-center flex-column gap-5">
-   
-      <Main/>
-      <Card/>
-      <Contributions/>
-      <Contact/>
-      
+      <Main />
+      <Card />
+      <Contributions />
+      <Contact />
     </div>
   );
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-   
-            <div className='col-12 d-flex flex-column align-items-center gap-4'>
-                            <span className='text-white fs-4 fw-light p-1'>Interested in delving deeper into the project?</span>
-                            <span className='text-white fs-6 fw-light text-center cont-font'>If you'd like to explore further details about our initiatives or any of our affiliated brands, don't hesitate to connect. You can reach out to us via email at hello@abc.com or give us a call at +91 480 20802730.</span>
- <div className='d-flex col-12 flex-sm-row flex-column gap-3 align-items-center justify-content-center'>
-    <button className='border-light text-white  cont-ring-btn'>Ring us on Skype</button>
-    <button className='border-0 text-black contact-btn'>Contact Us</button>
- </div>
+    <div className="col-12 d-flex flex-column align-items-center gap-4">
+      <span className="text-white fs-4 fw-light p-1">
+        Interested in delving deeper into the project?
+      </span>
+      <span className="text-white fs-6 fw-light text-center cont-font">
+        If you'd like to explore further details about our initiatives or any of
+        our affiliated brands, don't hesitate to connect. You can reach out to
+        us via email at hello@abc.com or give us a call at +91 480 20802730.
+      </span>
+      <div className="d-flex col-12 flex-sm-row flex-column gap-3 align-items-center justify-content-center">
+        <button className="border-light text-white  cont-ring-btn">
+          Ring us on Skype
+        </button>
+        <button className="border-0 text-black contact-btn">Contact Us</button>
+      </div>
 
- <span className='fw-lighter  text-white mt-5 foot-font'> 2019-2023 abcTechnology Solutions Pvt. Ltd. All Rights Reserved</span>
+      <span className="fw-lighter  text-white mt-5 foot-font">
+        {" "}
+        2019-2023 abcTechnology Solutions Pvt. Ltd. All Rights Reserved
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
